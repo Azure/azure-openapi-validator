2eae332faea7718bdc50789187792b9d489a3f57
Deepak Shankargouda <deshank@microsoft.com>
2017-05-26 14:01:49 -0700

    Minor comments

--- src/azure-openapi-validator/rules/ArraySchemaMustHaveItems.ts
+++ src/azure-openapi-validator/rules/ArraySchemaMustHaveItems.ts
@@ -18,7 +18,11 @@ rules.push({
   run: function* (doc, node, path) {
     const msg: string = "Please provide an items property for array type: ";
     if (node === 'array') {
+      // climb up the json path to get hold of the parent element
+      // check if the parent element has an items node defined
       const propObject = jp.query(doc, path.splice(0, path.length - 2).join('.'));
+
+      // if not, we have a violation
       if (!(propObject.hasOwnProperty('items'))) {
         yield { message: `${msg} ${path[path.length - 2]}`, location: path.splice(0, path.length - 2) };
       }
