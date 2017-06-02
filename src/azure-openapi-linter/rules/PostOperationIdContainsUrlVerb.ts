2b66e5f0e3efffc9f4cf3370e8ae204ec0736fce
Deepak Shankargouda <deshank@microsoft.com>
2017-06-02 12:04:58 -0700

    BGetter node querying

--- src/azure-openapi-linter/rules/PostOperationIdContainsUrlVerb.ts
+++ src/azure-openapi-linter/rules/PostOperationIdContainsUrlVerb.ts
@@ -12,15 +12,24 @@ rules.push({
   mergeState: MergeStates.individual,
   openapiType: OpenApiTypes.arm,
 
-  appliesTo_JsonQuery: "$.paths.*.post.operationId",
+  appliesTo_JsonQuery: "$.paths[?(@.post)]",
   run: function* (doc, node, path) {
+    // path array returned here will be of the form ['paths', 'some/path']
+    // pick the last element to get hold of the actual path
     // get the url
-    const urlVerb = (<string>path[path.length - 3]).split('/').pop();
+    const urlVerb = (<string>path[path.length - 1]).toLowerCase();
+    // now get hold of the operation id
+    const keys = Object.keys(node);
+    const postKey = keys.find(key => {
+      return key.toLowerCase() === 'post';
+    });
+    const operationId = node[postKey].operationId;
+    // operationIds are of the form Noun_Verb, get hold of the verb so we can perform
+    // the check
     // check if we have an operation id without the verb at the end of the url
     // if not, this should be a violation
-    if (node.toLowerCase().split('_').pop().indexOf(urlVerb) === -1) {
+    if (operationId.toLowerCase().split('_').pop().indexOf(urlVerb) === -1) {
       yield { message: `OperationId must contain the verb: '${urlVerb}' in:'${node}'`, location: path };
     }
   }
-
 });
