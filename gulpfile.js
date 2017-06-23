3272bc882814cc291ad8ab97b18411aec60ed19b
Deepak Shankargouda <deshank@microsoft.com>
2017-06-22 17:21:44 -0700

    gulp stash 1

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,19 @@ var clean = require('gulp-clean');
 var run = require('gulp-run')
 var mocha = require('gulp-mocha');
 
-gulp.task('clean', function () {
+// All the typescript tasks
+gulp.task('clean/typescript', function () {
     console.log('Cleaning build directories...');
-    return gulp.src('src/**/*.{js,d.ts}', { read: false })
+    return gulp.src('src/typescript/**/*.{js,d.ts}', { read: false })
         .pipe(clean({ force: true }));
 });
 
-gulp.task('build', ['clean'], function () {
-    console.log('Running the Typescript transpiler...');
-    return gulp.src('./src/').pipe(run('tsc --project tsconfig.json'));
+gulp.task('build/typescript', ['clean/typescript'], function () {
+    console.log('Running the typescript transpiler...');
+    return gulp.src('./src/typescript/').pipe(run('tsc --project tsconfig.json'));
 });
 
-gulp.task('test', ['build'], function () {
+gulp.task('test/typescript', ['build/typescript'], function () {
     console.log('Running the unit tests...');
     gulp.src(['src/typescript/azure-openapi-validator/tests/*.js'])
         .pipe(mocha({
@@ -23,6 +24,39 @@ gulp.task('test', ['build'], function () {
         .once('error', () => {
             process.exit(1);
         });
+});
+
+// All the dotnet tasks
+gulp.task('clean/dotnet', function () {
+    console.log('Cleaning build directories...');
+    return gulp.src('src/dotnet/**/bin', { read: false })
+        .pipe(clean({ force: true }));
+});
+
+gulp.task('restore/dotnet', ['clean/dotnet'], function () {
+    console.log('Running dotnet restore...');
+    return gulp.src('./src/dotnet/*.csproj')
+        .pipe(run('dotnet restore'));
+});
+
+gulp.task('build/dotnet', ['restore/dotnet'], function () {
+    console.log('Running dotnet build...');
+    return gulp.src('./src/dotnet/*.csproj')
+        .pipe(run('dotnet build'));
+});
+
+gulp.task('test/dotnet', ['build/dotnet'], function () {
+    console.log('Running the dotnet unit tests...');
+    gulp.src(['./src/dotnet/AutoRest.Swagger.Tests/AutoRest.Swagger.Tests.csproj'])
+        .pipe(run('dotnet test -v'))
+        .once('error', () => {
+            process.exit(1);
+        });
+});
+
+// Now the defaults
+gulp.task('test', ['test/dotnet', 'test/typescript'], function () {
+    console.log('Running the unit tests...');
 });
 
 gulp.task('default', function () {
