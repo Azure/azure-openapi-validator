73b23ec6c99e5e038816f4bd2e0d01256fd27d6f
George Pollard <porges@porg.es>
2017-09-08 11:27:08 +1000

    Improve the result of GetCamelCasedSuggestion (#87)
    * Improve the result of GetCamelCasedSuggestion
    
    `GetCamelCasedSuggestion` returns:
    
    * `nodeAgentSkUid` for `nodeAgentSKUId`
    * `inboundNaTpools` for `inboundNATPools`
    
    This change improves them to:
    
    * `nodeAgentSkuId` for `nodeAgentSKUId`
    * `inboundNatPools` for `inboundNATPools`
    
    * Clarify GetCamelCasedSuggestion regex, and add tests for behaviour

---
+++ src/dotnet/OpenAPI.Validator.Tests/GetCamelCasedSuggestionTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenAPI.Validator.Model.Utilities;
+using Xunit;
+
+namespace OpenAPI.Validator.Tests
+{
+    public class GetCamelCasedSuggestionTests
+    {
+        [Theory]
+        [InlineData("URI", "Uri")]
+        [InlineData("SomeURIThing", "SomeUriThing")]
+        [InlineData("IP", "Ip")]
+        [InlineData("IPAddress", "IpAddress")]
+        public void CorrectlyCasesExamples(string input, string expectedOutput)
+        {
+            Assert.Equal(expectedOutput, ValidationUtilities.GetCamelCasedSuggestion(input));
+        }
+    }
+}
