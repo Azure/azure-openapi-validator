817d5be6bc5af14c4b126361083be642904456a5
Deepak Shankargouda <deshank@microsoft.com>
2017-09-13 16:35:04 -0700

    More AutoRest package fix

--- gulpfile.js
+++ gulpfile.js
@@ -97,5 +97,5 @@ gulp.task('default', ['dotnet', 'typescript'], function () {
 gulp.task('dotnet/pack', ['dotnet', 'typescript'], function () {
     console.log('Kicking off the dotnet publish task...');
     return gulp.src('src/dotnet/AutoRest/AutoRest.csproj')
-        .pipe(publish({ configuration: 'release', output: 'src/dotnet/AutoRest/bin/netcoreapp2.0' }));
+        .pipe(publish({ configuration: 'release', output: '/bin/netcoreapp2.0' }));
 });
