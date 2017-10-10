4997c9eff040d7c17016da1cf58e7d6e73ad7a79
Deepak Shankargouda <deshank@microsoft.com>
2017-10-09 17:44:55 -0700

    minot test

--- src/typescript/azure-openapi-validator/tests/individual-azure-tests.ts
+++ src/typescript/azure-openapi-validator/tests/individual-azure-tests.ts
@@ -2,19 +2,15 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+
+import { suite, test } from 'mocha-typescript';
+import * as assert from "assert";
 import { Message } from '../../jsonrpc/types';
-import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
-import { safeLoad } from "js-yaml";
-import { AutoRestPluginHost } from "../../jsonrpc/plugin-host";
-import { run } from "../../azure-openapi-validator";
-import {
-  assertValidationRuleCount,
-  collectTestMessagesFromValidator
-} from './utilities/tests-helper';
 import { MergeStates, OpenApiTypes } from '../rule';
 import { ControlCharactersAreNotAllowed } from '../rules/ControlCharactersAreNotAllowed';
-import { PostOperationIdContainsUrlVerb } from '../rules/PostOperationIdContainsUrlVerb';
 import { LicenseHeaderMustNotBeSpecified } from '../rules/LicenseHeaderMustNotBeSpecified';
+import { PostOperationIdContainsUrlVerb } from '../rules/PostOperationIdContainsUrlVerb';
+import { assertValidationRuleCount, collectTestMessagesFromValidator } from './utilities/tests-helper';
 
 @suite class IndividualAzureTests {
   @test async "control characters not allowed test"() {
@@ -27,6 +23,7 @@ import { LicenseHeaderMustNotBeSpecified } from '../rules/LicenseHeaderMustNotBe
     const fileName = 'PostOperationIdWithoutUrlVerb.json';
     const messages: Message[] = await collectTestMessagesFromValidator(fileName, OpenApiTypes.arm, MergeStates.individual);
     assertValidationRuleCount(messages, PostOperationIdContainsUrlVerb, 1);
+	assert(messages[0].Text === "OperationId should contain the verb: 'invoke' in:'simpleManualTrigger_call'. Consider updating the operationId");
   }
   @test async "info section with x-ms-code-generation-settings must not contain a header"() {
     const fileName = 'InfoWithLicenseHeader.json';
