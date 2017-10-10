dfb276e4e10a817c51980b79285760f7469106d5
Deepak Shankargouda <deshank@microsoft.com>
2017-10-09 17:46:43 -0700

    nunused

--- src/typescript/azure-openapi-validator/tests/individual-azure-tests.ts
+++ src/typescript/azure-openapi-validator/tests/individual-azure-tests.ts
@@ -2,15 +2,20 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-
-import { suite, test } from 'mocha-typescript';
-import * as assert from "assert";
 import { Message } from '../../jsonrpc/types';
+import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
+import { safeLoad } from "js-yaml";
+import { AutoRestPluginHost } from "../../jsonrpc/plugin-host";
+import { run } from "../../azure-openapi-validator";
+import {
+  assertValidationRuleCount,
+  collectTestMessagesFromValidator
+} from './utilities/tests-helper';
 import { MergeStates, OpenApiTypes } from '../rule';
 import { ControlCharactersAreNotAllowed } from '../rules/ControlCharactersAreNotAllowed';
-import { LicenseHeaderMustNotBeSpecified } from '../rules/LicenseHeaderMustNotBeSpecified';
 import { PostOperationIdContainsUrlVerb } from '../rules/PostOperationIdContainsUrlVerb';
-import { assertValidationRuleCount, collectTestMessagesFromValidator } from './utilities/tests-helper';
+import { LicenseHeaderMustNotBeSpecified } from '../rules/LicenseHeaderMustNotBeSpecified';
+import * as assert from "assert";
 
 @suite class IndividualAzureTests {
   @test async "control characters not allowed test"() {
@@ -23,7 +28,7 @@ import { assertValidationRuleCount, collectTestMessagesFromValidator } from './u
     const fileName = 'PostOperationIdWithoutUrlVerb.json';
     const messages: Message[] = await collectTestMessagesFromValidator(fileName, OpenApiTypes.arm, MergeStates.individual);
     assertValidationRuleCount(messages, PostOperationIdContainsUrlVerb, 1);
-	assert(messages[0].Text === "OperationId should contain the verb: 'invoke' in:'simpleManualTrigger_call'. Consider updating the operationId");
+	assert(messages[0].Text === "OperationId should contain the verb: 'invoke' in:'simpleManualTrigger_call'. Consider updating the operationId"); 
   }
   @test async "info section with x-ms-code-generation-settings must not contain a header"() {
     const fileName = 'InfoWithLicenseHeader.json';
