bee61da5a3246bed1fdf887ecfe8791ccab0358a
JianyeXi <59603451+jianyexi@users.noreply.github.com>
2020-04-11 20:58:05 +0800

    Rule R4005 XmsEnumNameUnique (#200)
    * add rule XmsEnumNameUnique
    
    * update msg
    
    * update category & test

--- src/typescript/azure-openapi-validator/index.ts
+++ src/typescript/azure-openapi-validator/index.ts
@@ -19,6 +19,7 @@ require("./rules/OperationIdRequired");
 require("./rules/EnumMustHaveType");
 require("./rules/EnumUniqueValue");
 require("./rules/EnumMustNotHaveEmptyValue");
+require("./rules/UniqueXmsEnumName");
 require("./rules/DeprecatedXmsCodeGenerationSetting");
 
 export function run(
---
+++ src/typescript/azure-openapi-validator/rules/UniqueXmsEnumName.ts
@@ -0,0 +1,45 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { MergeStates, OpenApiTypes, rules } from "../rule";
+export const UniqueXmsEnumName: string = "UniqueXmsEnumName";
+import { nodes } from "jsonpath";
+
+var enumList: string[] = []
+
+rules.push({
+  id: "R4005",
+  name: UniqueXmsEnumName,
+  severity: "error",
+  category: "SDKViolation",
+  mergeState: MergeStates.composed,
+  openapiType: OpenApiTypes.arm | OpenApiTypes.dataplane,
+  appliesTo_JsonQuery: "$.definitions",
+  run: function* (doc, node, path) {
+    const msg: string = `Must not have duplicate name of x-ms-enum extension , make sure every x-ms-enum name unique.`;
+    if (node) {
+      let enumList: string[] = []
+      for (const section of nodes(
+        node,
+        "$..[\"x-ms-enum\"]"
+      )) {
+        if (section.value.name) {
+          enumList.push(section.value.name);
+        }
+      }
+      const caseInsensitiveSet = new Set<string>();
+      if (
+        enumList.some(value => {
+          if (caseInsensitiveSet.has(value.toLowerCase())) {
+            return true;
+          }
+          caseInsensitiveSet.add(value.toLowerCase());
+          return false;
+        })
+      ) {
+        yield { message: `${msg}`, location: path };
+      }
+    }
+  }
+});
