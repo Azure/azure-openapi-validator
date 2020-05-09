d220aba6c8b9cc7f557c7410c9ecf102bb54a5c7
Jianye Xi <jianye.xi@microsoft.com>
2020-05-09 18:48:06 +0800

    improve R4005

--- src/typescript/azure-openapi-validator/rules/UniqueXmsEnumName.ts
+++ src/typescript/azure-openapi-validator/rules/UniqueXmsEnumName.ts
@@ -6,7 +6,7 @@ import { MergeStates, OpenApiTypes, rules } from "../rule";
 export const UniqueXmsEnumName: string = "UniqueXmsEnumName";
 import { nodes } from "jsonpath";
 
-var enumList: string[] = []
+var enumList: string[] = [];
 
 rules.push({
   id: "R4005",
@@ -17,29 +17,28 @@ rules.push({
   openapiType: OpenApiTypes.arm | OpenApiTypes.dataplane,
   appliesTo_JsonQuery: "$.definitions",
   run: function* (doc, node, path) {
-    const msg: string = `Must not have duplicate name of x-ms-enum extension , make sure every x-ms-enum name unique.`;
+    const msg: string = `Must not have duplicate name of x-ms-enum extension, make sure every x-ms-enum name unique. The x-ms-enum name is duplicate :`;
     if (node) {
-      let enumList: string[] = []
-      for (const section of nodes(
-        node,
-        "$..[\"x-ms-enum\"]"
-      )) {
+      let enumList: string[] = [];
+      for (const section of nodes(node, '$..["x-ms-enum"]')) {
         if (section.value.name) {
           enumList.push(section.value.name);
         }
       }
       const caseInsensitiveSet = new Set<string>();
+      let enumName: string = "";
       if (
-        enumList.some(value => {
+        enumList.some((value) => {
           if (caseInsensitiveSet.has(value.toLowerCase())) {
+            enumName = value;
             return true;
           }
           caseInsensitiveSet.add(value.toLowerCase());
           return false;
         })
       ) {
-        yield { message: `${msg}`, location: path };
+        yield { message: `${msg} ${enumName}`, location: path };
       }
     }
-  }
+  },
 });
