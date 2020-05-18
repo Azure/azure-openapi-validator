c6244df3f76f7fd3e273eb05aa2a3f0bcaeba55b
Jianye Xi <jianyxi@microsoft.com>
2020-05-18 15:12:41 +0800

    adding new rule: R4010

---
+++ src/typescript/azure-openapi-validator/rules/RequiredDefaultResponse.ts
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import { MergeStates, OpenApiTypes, rules } from "../rule"
+export const RequiredDefaultResponse: string = "RequiredDefaultResponse"
+
+const jp = require("jsonpath")
+
+rules.push({
+  id: "R4010",
+  name: RequiredDefaultResponse,
+  severity: "error",
+  category: "ARMViolation",
+  mergeState: MergeStates.individual,
+  openapiType: OpenApiTypes.arm,
+  appliesTo_JsonQuery: "$.paths..responses",
+  *run(doc, node, path) {
+    if (!node.default) {
+      yield {
+        message: `The response is defined but without a default error response implementing.Consider adding it.'`,
+        location: path
+      }
+    }
+  }
+})
