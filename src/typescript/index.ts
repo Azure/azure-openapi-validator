5dd7f93fea43cb5f3bb753fa417c4dc7e7acae48
jianyexi <jianyxi@microsoft.com>
2020-08-13 17:27:07 +0800

    improve

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -29,7 +29,7 @@ async function main() {
           file,
           openapiDefinitionObject,
           initiator.Message.bind(initiator),
-          subType === "rpaas" ? OpenApiTypes[subType] : OpenApiTypes[openapiType],
+          subType === "rpaas" ? OpenApiTypes[subType] | OpenApiTypes[openapiType] : OpenApiTypes[openapiType],
           MergeStates[mergeState]
         )
       } catch (e) {
