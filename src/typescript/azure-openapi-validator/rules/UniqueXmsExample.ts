da173ce0408fa268553137d3deab6174f520091a
ruowan <ruowan@microsoft.com>
2020-11-27 06:02:39 +0000

    optimise UniqueXmsExample rule

--- src/typescript/azure-openapi-validator/rules/UniqueXmsExample.ts
+++ src/typescript/azure-openapi-validator/rules/UniqueXmsExample.ts
@@ -12,13 +12,14 @@ rules.push({
   openapiType: OpenApiTypes.arm | OpenApiTypes.dataplane,
   appliesTo_JsonQuery: "$",
   *run(doc, node, path) {
-    const msg: string = `Do not have duplicate name of x-ms-example, make sure every x-ms-example name unique.`
+    const msg: string = `Do not have duplicate name of x-ms-example, make sure every x-ms-example name unique. Duplicate x-ms-example: `
     const examples = new Set<string>()
     for (const it of nodes(node, '$.paths.*.*["x-ms-examples"]')) {
       if (it.value !== null) {
         for (const exampleName of Object.keys(it.value)) {
+          console.log(exampleName)
           if (examples.has(exampleName)) {
-            yield { message: msg, location: path.concat(it.path.slice(1)) }
+            yield { message: msg + exampleName, location: path.concat(it.path.slice(1)) }
           }
           examples.add(exampleName)
         }
