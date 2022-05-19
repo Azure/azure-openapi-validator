f5ff11e0a839b861234d4dec33687ffdd0dfd339
Jianye Xi <jianye.xi@microsoft.com>
2022-05-19 15:30:54 +0800

    validator core bugfix

--- packages/azure-openapi-validator/core/src/swaggerInventory.ts
+++ packages/azure-openapi-validator/core/src/swaggerInventory.ts
@@ -1,16 +1,15 @@
 import * as _ from "lodash"
 import { OpenapiDocument } from "./document"
 import { JsonParser } from "./jsonParser"
-import { ISwaggerInventory,IFileSystem } from "./types"
+import { ISwaggerInventory, IFileSystem } from "./types"
 import { defaultFileSystem, normalizePath } from "./utils"
 const DepGraph = require("dependency-graph").DepGraph
 export class SwaggerInventory implements ISwaggerInventory {
   private inventory = new DepGraph()
   private referenceCache = new Map<string, OpenapiDocument>()
   private allDocs = new Map<string, any>()
-  private docRecords:Record<string,any>|undefined = undefined
-  constructor(private fileSystem:IFileSystem = defaultFileSystem){
-  }
+  private docRecords: Record<string, any> | undefined = undefined
+  constructor(private fileSystem: IFileSystem = defaultFileSystem) {}
 
   public getSingleDocument(specPath: string) {
     return this.getInternalDocument(specPath)?.getObj()
@@ -28,8 +27,8 @@ export class SwaggerInventory implements ISwaggerInventory {
     throw new Error(`No cached file:${specPath}`)
   }
 
-  public referencesOf(specPath: string): Record<string,any>{
-    const result :Record<string,any> = []
+  public referencesOf(specPath: string): Record<string, any> {
+    const result: Record<string, any> = {}
     const references = this.inventory.dependantsOf(normalizePath(specPath))
     for (const ref of references) {
       result[ref] = this.allDocs.get(ref)
@@ -37,18 +36,17 @@ export class SwaggerInventory implements ISwaggerInventory {
     return result
   }
 
-  public getDocuments(docPath?:string): Record<string, any> | any {
+  public getDocuments(docPath?: string): Record<string, any> | any {
     if (docPath) {
       return this.getSingleDocument(docPath)
     }
     if (!this.docRecords) {
       this.docRecords = {}
-      for (const [key,value] of this.allDocs.entries()) {
+      for (const [key, value] of this.allDocs.entries()) {
         this.docRecords[key] = value
       }
     }
     return this.docRecords
-   
   }
 
   async loadDocument(specPath: string): Promise<any> {
@@ -67,17 +65,19 @@ export class SwaggerInventory implements ISwaggerInventory {
       return cache
     }
     const parser = new JsonParser()
-    const document = new OpenapiDocument(specPath, parser,this.fileSystem)
+    const document = new OpenapiDocument(specPath, parser, this.fileSystem)
     await document.resolve()
     this.referenceCache.set(specPath, document)
-    this.allDocs.set(specPath,document.getContent())
+    this.allDocs.set(specPath, document.getContent())
+    this.inventory.addNode(specPath)
     const references = document.getReferences()
     for (const ref of references) {
       if (!this.allDocs.has(ref)) {
+        this.inventory.addNode(ref)
+        this.inventory.addDependency(specPath, ref)
         await this.cacheDocument(ref)
       }
     }
     return document
   }
-
 }
