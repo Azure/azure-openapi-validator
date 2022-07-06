8ff1adeb3f8159d8914dc2a0dc96214ef497e106
jianye xi <jianyxi@microsoft.com>
2022-07-06 14:12:43 +0800

    update js ruleset

--- packages/rulesets/generated/spectral/az-arm.js
+++ packages/rulesets/generated/spectral/az-arm.js
@@ -298,6 +298,27 @@ const provisioningState = (swaggerObj, _opts, paths) => {
     return [];
 };
 
+const hasHeader = (response, opts, paths) => {
+    if (response === null || typeof response !== 'object') {
+        return [];
+    }
+    if (opts === null || typeof opts !== 'object' || !opts.name) {
+        return [];
+    }
+    const path = paths.path || [];
+    const hasHeader = Object.keys(response.headers || {})
+        .some((name) => name.toLowerCase() === opts.name.toLowerCase());
+    if (!hasHeader) {
+        return [
+            {
+                message: `Response should include an "${opts.name}" response header.`,
+                path: [...path, 'headers'],
+            },
+        ];
+    }
+    return [];
+};
+
 const ruleset = {
     extends: [ruleset$1],
     rules: {
