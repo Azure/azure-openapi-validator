13c01f1eebdd1d43e50bf0c84fe44e825d67931e
jianyexi <jianyxi@microsoft.com>
2022-10-13 14:21:03 +0800

    fix xmsExampleRequired

--- packages/rulesets/src/spectral/functions/xms-examples-required.ts
+++ packages/rulesets/src/spectral/functions/xms-examples-required.ts
@@ -1,17 +1,16 @@
 // Verifies whether `x-ms-examples` are provided for each operation or not.
-const xmsExamplesRequired = (swaggerObj:any, _opts:any, paths:any) => {
-    if (swaggerObj === null || typeof swaggerObj !== "object") {
-        return [];
-    }
-    if (swaggerObj['x-ms-examples'] !== undefined)
-        return [];
-    const path = paths.path || [];
-    return [
-        {
-            message: `Please provide x-ms-examples describing minimum/maximum property set for response/request payloads for operations.`,
-            path: path
-        },
-    ];
-};
+const xmsExamplesRequired = (swaggerObj: any, _opts: any, paths: any) => {
+  if (swaggerObj === null || typeof swaggerObj !== "object") {
+    return []
+  }
+  if (swaggerObj["x-ms-examples"] !== undefined && Object.keys(swaggerObj["x-ms-examples"].length > 0)) return []
+  const path = paths.path || []
+  return [
+    {
+      message: `Please provide x-ms-examples describing minimum/maximum property set for response/request payloads for operations.`,
+      path: path,
+    },
+  ]
+}
 
 export default xmsExamplesRequired
