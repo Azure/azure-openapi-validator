3764de3095aed065d738a5428a5d9399be1a9aa3
jianye xi <jianyxi@microsoft.com>
2023-01-30 14:08:57 +0800

    fix two rules

--- packages/rulesets/src/spectral/functions/Extensions/long-running-response-status-code.ts
+++ packages/rulesets/src/spectral/functions/Extensions/long-running-response-status-code.ts
@@ -1,42 +1,33 @@
 // An x-ms-long-running-operation extension passes this rule if the operation that this extension has a valid response defined.
 
-const longRunningResponseStatusCode = (
-  methodOp: any,
-  _opts: any,
-  ctx: any,
-  validResponseCodesList: any
-) => {
+const longRunningResponseStatusCode = (methodOp: any, _opts: any, ctx: any, validResponseCodesList: any) => {
   if (methodOp === null || typeof methodOp !== "object") {
-    return [];
+    return []
   }
-  const path = ctx.path || [];
-  const errors: any = [];
-  const method = Object.keys(methodOp)[0];
+  const path = ctx.path || []
+  const errors: any = []
+  const method = Object.keys(methodOp)[0]
   if (!["delete", "put", "patch", "post"].includes(method)) {
-    return [];
+    return []
   }
-  const operationId = methodOp?.[method]?.operationId || "";
+  const operationId = methodOp?.[method]?.operationId || ""
   if (!methodOp?.[method]?.["x-ms-long-running-operation"]) {
-    return [];
+    return []
   }
   if (methodOp?.[method]?.responses) {
-    const responseCodes = Object.keys(methodOp?.[method]?.responses);
-    const validResponseCodes = validResponseCodesList[method];
-    const validResponseCodeString = validResponseCodes.join(" or ");
-    for (const responseCode of responseCodes) {
-      if (
-        (responseCodes.length === 1 && !validResponseCodes.includes(responseCode)) ||
-        (responseCode !== "default" && !validResponseCodes.includes(responseCode))
-      ) {
-        errors.push({
-          message: `A '${method}' operation '${operationId}' with x-ms-long-running-operation extension must have a valid terminal success status code ${validResponseCodeString}.`,
-          path: [...path, method],
-        });
-      }
+    const responseCodes = Object.keys(methodOp?.[method]?.responses)
+    const validResponseCodes = validResponseCodesList[method]
+    const validResponseCodeString = validResponseCodes.join(" or ")
+    const withTerminalCode = validResponseCodes.some((code: string) => responseCodes.includes(code))
+    if (!withTerminalCode) {
+      errors.push({
+        message: `A '${method}' operation '${operationId}' with x-ms-long-running-operation extension must have a valid terminal success status code ${validResponseCodeString}.`,
+        path: [...path, method],
+      })
     }
   }
-  return errors;
-};
+  return errors
+}
 
 export const longRunningResponseStatusCodeArm = (methodOp: any, _opts: any, ctx: any) => {
   const validResponseCodesList: any = {
@@ -44,9 +35,9 @@ export const longRunningResponseStatusCodeArm = (methodOp: any, _opts: any, ctx:
     post: ["200", "201", "202", "204"],
     put: ["200", "201"],
     patch: ["200", "201", "202"],
-  };
-  return longRunningResponseStatusCode(methodOp, _opts, ctx, validResponseCodesList);
-};
+  }
+  return longRunningResponseStatusCode(methodOp, _opts, ctx, validResponseCodesList)
+}
 
 export const longRunningResponseStatusCodeDataPlane = (methodOp: any, _opts: any, ctx: any) => {
   const validResponseCodesList: any = {
@@ -54,6 +45,6 @@ export const longRunningResponseStatusCodeDataPlane = (methodOp: any, _opts: any
     post: ["200", "201", "202", "204"],
     put: ["200", "201", "202"],
     patch: ["200", "201", "202"],
-  };
-  return longRunningResponseStatusCode(methodOp, _opts, ctx, validResponseCodesList);
-};
+  }
+  return longRunningResponseStatusCode(methodOp, _opts, ctx, validResponseCodesList)
+}
