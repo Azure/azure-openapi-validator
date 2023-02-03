69ab027ac677e557a34f82fe275e95ea1dbf57a1
brettdefoy <brettdefoy@microsoft.com>
2023-02-02 16:56:55 -0800

    Add more tests

--- packages/rulesets/src/spectral/test/no-duplicate-paths-for-scope-parameter.test.ts
+++ packages/rulesets/src/spectral/test/no-duplicate-paths-for-scope-parameter.test.ts
@@ -53,13 +53,63 @@ test("NoDuplicatePathsForScopeParameter should find errors", () => {
   })
 })
 
-// test("NoDuplicatePathsForScopeParameter should find no errors", () => {
-//   const oasDoc = {
-//     swagger: "2.0",
-//     paths: {},
-//     parameters: {},
-//   }
-//   return linter.run(oasDoc).then((results) => {
-//     expect(results.length).toBe(0)
-//   })
-// })
+test("NoDuplicatePathsForScopeParameter should find no errors with scope parameter", () => {
+  const oasDoc = {
+    swagger: "2.0",
+    paths: {
+      "/{scope}/providers/Microsoft.Bakery/breads": {
+        get: {
+          parameters: [{ $ref: "#/parameters/ScopeParameter" }],
+        },
+      },
+    },
+    parameters: {
+      ScopeParameter: {
+        name: "scope",
+        in: "path",
+        required: true,
+      },
+    },
+  }
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(0)
+  })
+})
+
+test("NoDuplicatePathsForScopeParameter should find no errors with explicitly defined scopes", () => {
+  const oasDoc = {
+    swagger: "2.0",
+    paths: {
+      "/subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/Microsoft.Bakery/breads": {
+        get: {
+          parameters: [{ $ref: "#/parameters/SubscriptionIdParameter" }, { $ref: "#/parameters/ResourceGroupParameter" }],
+        },
+      },
+      "/subscriptions/{subscriptionId}/providers/Microsoft.Bakery/breads": {
+        get: {
+          parameters: [{ $ref: "#/parameters/SubscriptionIdParameter" }],
+        },
+      },
+    },
+    parameters: {
+      SubscriptionIdParameter: {
+        name: "subscriptionId",
+        in: "path",
+        required: true,
+      },
+      ResourceGroupParameter: {
+        name: "resourceGroupName",
+        in: "path",
+        required: true,
+      },
+      ScopeParameter: {
+        name: "scope",
+        in: "path",
+        required: true,
+      },
+    },
+  }
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(0)
+  })
+})
