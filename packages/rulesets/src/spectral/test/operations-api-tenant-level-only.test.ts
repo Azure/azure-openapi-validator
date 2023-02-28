b5d54e0d5d85a4697dd6f99935e3ca15223723c2
Brett DeFoy <brettdefoy@microsoft.com>
2023-02-27 19:18:14 -0800

    Add more to test

--- packages/rulesets/src/spectral/test/operations-api-tenant-level-only.test.ts
+++ packages/rulesets/src/spectral/test/operations-api-tenant-level-only.test.ts
@@ -15,14 +15,20 @@ test("OperationsApiTenantLevelOnly should find errors", () => {
       "/providers/Microsoft.LoadTestService/operations": {},
       "/subscriptions/{subscriptionId}/providers/Microsoft.LoadTestService/operations": {},
       "/providers/Microsoft.LoadTestService/subscriptions/{subscriptionId}/operations": {},
+      "/subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/Microsoft.LoadTestService/operations": {},
       "/subscriptions/{subscriptionId}/providers/Microsoft.LoadTestService/loadTests": {},
       "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.LoadTestService/loadTests": {},
       "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.LoadTestService/loadTests/{loadTestName}": {},
     },
   }
   return linter.run(oasDoc).then((results) => {
-    expect(results.length).toBe(1)
+    expect(results.length).toBe(2)
+    expect(results[0].message).toBe("The operations API must only be at the tenant level.")
     expect(results[0].path.join(".")).toBe("paths./subscriptions/{subscriptionId}/providers/Microsoft.LoadTestService/operations")
+    expect(results[1].message).toBe("The operations API must only be at the tenant level.")
+    expect(results[1].path.join(".")).toBe(
+      "paths./subscriptions/{subscriptionId}/resourcegroups/{resourceGroupName}/providers/Microsoft.LoadTestService/operations"
+    )
   })
 })
 
