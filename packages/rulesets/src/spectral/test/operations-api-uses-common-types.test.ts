2dc8547703f6d5707e43ea2cd4cab3e77df644b5
Brett DeFoy <brettdefoy@microsoft.com>
2023-03-22 14:43:21 -0700

    Cleanup and add test

--- packages/rulesets/src/spectral/test/operations-api-uses-common-types.test.ts
+++ packages/rulesets/src/spectral/test/operations-api-uses-common-types.test.ts
@@ -6,11 +6,7 @@ beforeAll(async () => {
   nonResolvingLinter = await linterForRule("OperationsApiSchemaUsesCommonTypes", true)
 })
 
-test("OperationsApiSchemaUsesCommonTypes should find errors for invalid path", () => {
-  // invalid paths:
-  //  1 <scope>/providers/Microsoft.Compute/{vmName}
-  //  2 <scope>/providers/{resourceName}/Microsoft.MyNs...
-  //  3 <scope>/providers/ResourceType/Microsoft.MyNs...
+test("OperationsApiSchemaUsesCommonTypes should find errors when using redefined common type", () => {
   const oasDoc = {
     swagger: "2.0",
     paths: {
@@ -58,11 +54,34 @@ test("OperationsApiSchemaUsesCommonTypes should find errors for invalid path", (
     expect(results[0].message).toContain("Operations API path must follow the schema provided in the common types.")
   })
 })
+
+test("OperationsApiSchemaUsesCommonTypes should find errors when using redefined common type in another file", () => {
+  const oasDoc = {
+    swagger: "2.0",
+    paths: {
+      "/providers/Microsoft.MyNs/operations": {
+        get: {
+          tags: ["SampleTag"],
+          operationId: "Foo_CreateOrUpdate",
+          description: "Test Description",
+          parameters: [],
+          responses: {
+            "200": {
+              schema: { $ref: "#/../not-common-types/path.json#/definitions/OperationListResult" },
+            },
+          },
+        },
+      },
+    },
+  }
+  return nonResolvingLinter.run(oasDoc).then((results: any) => {
+    expect(results.length).toBe(1)
+    expect(results[0].path.join(".")).toBe("paths./providers/Microsoft.MyNs/operations.get.responses.200.schema.$ref")
+    expect(results[0].message).toContain("Operations API path must follow the schema provided in the common types.")
+  })
+})
+
 test("OperationsApiSchemaUsesCommonTypes should find no errors", () => {
-  // invalid paths:
-  //  1 <scope>/providers/Microsoft.Compute/{vmName}
-  //  2 <scope>/providers/{resourceName}/Microsoft.MyNs...
-  //  3 <scope>/providers/ResourceType/Microsoft.MyNs...
   const oasDoc = {
     swagger: "2.0",
     paths: {
@@ -82,7 +101,6 @@ test("OperationsApiSchemaUsesCommonTypes should find no errors", () => {
     },
   }
   return nonResolvingLinter.run(oasDoc).then((results: any) => {
-    console.log(results)
     expect(results.length).toBe(0)
   })
 })
