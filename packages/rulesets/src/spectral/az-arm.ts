97e18b88238203d52bff1ffe25f783920f1beb44
AkhillaIlla <36493984+AkhilaIlla@users.noreply.github.com>
2023-04-04 07:37:22 -0500

    Refactor RPC-Async-V1-02 to implement for put, patch & delete (#447)
    * with-xms-resource.test.ts
    
    Implement RPC-Put-V1-24 (ResponseSchemaSpecifiedForSuccessStatusCode)
    
    * minor updates
    
    * Implement RPC-Async-V1-02
    
    * update fixes
    
    * resolve rules.md indexing issue
    
    * fix CI lint errors
    
    * fix failing test
    
    * fix changelog error
    
    * updates based on comments
    
    * Fix failing lro-provisioning-state-specified rule
    
    * fix rush lint failures
    
    * refactor RPC-Async-V1-02 to implement for put, patch & delete
    
    * fix documentation
    
    * fix build failures
    
    * updates based on comments
    
    * split documentation, updates based on comments
    
    * add return codes to PUT error message
    
    * Fix test description, add additional test
    
    * Address comments
    
    ---------
    
    Co-authored-by: akhilailla <akhilailla@microsoft.com>

--- packages/rulesets/src/spectral/az-arm.ts
+++ packages/rulesets/src/spectral/az-arm.ts
@@ -11,10 +11,12 @@ import hasApiVersionParameter from "./functions/has-api-version-parameter"
 import hasheader from "./functions/has-header"
 import httpsSupportedScheme from "./functions/https-supported-scheme"
 import locationMustHaveXmsMutability from "./functions/location-must-have-xms-mutability"
+import provisioningStateSpecifiedForLRODelete from "./functions/lro-delete-provisioning-state-specified"
 import validateOriginalUri from "./functions/lro-original-uri"
 import { lroPatch202 } from "./functions/lro-patch-202"
+import provisioningStateSpecifiedForLROPatch from "./functions/lro-patch-provisioning-state-specified"
 import { lroPostReturn } from "./functions/lro-post-return"
-import provisioningStateSpecified from "./functions/lro-provisioning-state-specified"
+import provisioningStateSpecifiedForLROPut from "./functions/lro-put-provisioning-state-specified"
 import noDuplicatePathsForScopeParameter from "./functions/no-duplicate-paths-for-scope-parameter"
 import operationsApiSchema from "./functions/operations-api-schema"
 import { parameterNotDefinedInGlobalParameters } from "./functions/parameter-not-defined-in-global-parameters"
@@ -80,18 +82,52 @@ const ruleset: any = {
         function: longRunningResponseStatusCodeArm,
       },
     },
+
     // RPC Code: RPC-Async-V1-02
-    ProvisioningStateSpecified: {
-      description: 'A LRO PUT and PATCH operations response schema must have "ProvisioningState" property specified.',
+    //PUT
+    ProvisioningStateSpecifiedForLROPut: {
+      description:
+        'A LRO PUT operation\'s response schema must have "ProvisioningState" property specified for the 200 and 201 status codes.',
       message: "{{error}}",
       severity: "error",
       resolved: true,
       formats: [oas2],
-      given: "$[paths,'x-ms-paths'].*[put,patch].[?(@property === 'x-ms-long-running-operation' && @ === true)]^^",
+      given: ["$[paths,'x-ms-paths'].*[put][?(@property === 'x-ms-long-running-operation' && @ === true)]^"],
       then: {
-        function: provisioningStateSpecified,
+        function: provisioningStateSpecifiedForLROPut,
       },
     },
+    //Patch
+    ProvisioningStateSpecifiedForLROPatch: {
+      description:
+        'A long running Patch operation\'s response schema must have "ProvisioningState" property specified for the 200 status code.',
+      message: "{{error}}",
+      severity: "error",
+      resolved: true,
+      formats: [oas2],
+      given: [
+        "$[paths,'x-ms-paths'].*[patch][?(@property === 'x-ms-long-running-operation' && @ === true)]^.responses[?(@property == '200')]",
+      ],
+      then: {
+        function: provisioningStateSpecifiedForLROPatch,
+      },
+    },
+    //Delete
+    ProvisioningStateSpecifiedForLRODelete: {
+      description:
+        'A long running Delete operation\'s response schema must have "ProvisioningState" property specified for the 200 status code.',
+      message: "{{error}}",
+      severity: "warn",
+      resolved: true,
+      formats: [oas2],
+      given: [
+        "$[paths,'x-ms-paths'].*[delete][?(@property === 'x-ms-long-running-operation' && @ === true)]^.responses[?(@property == '200')]",
+      ],
+      then: {
+        function: provisioningStateSpecifiedForLRODelete,
+      },
+    },
+
     // https://github.com/Azure/azure-openapi-validator/issues/332
     // RPC Code: RPC-Async-V1-03
     ProvisioningStateValidation: {
