313a1710dcbcd937426bd26f4a46a0241152f5c4
Brett DeFoy <brettdefoy@microsoft.com>
2023-04-27 14:21:16 -0700

    Fix rule and add tests

--- packages/rulesets/src/spectral/functions/delete-response-codes.ts
+++ packages/rulesets/src/spectral/functions/delete-response-codes.ts
@@ -1,8 +1,8 @@
 // Synchronous DELETE must have 200 & 204 return codes and LRO DELETE must have 202 & 204 return codes.
 // RPC Code: RPC-Delete-V1-01
 
-const SYNC_DELETE_RESPONSES = ["202", "204", "default"]
-const LR_DELETE_RESPONSES = ["200", "204", "default"]
+const SYNC_DELETE_RESPONSES = ["200", "204", "default"]
+const LR_DELETE_RESPONSES = ["202", "204", "default"]
 
 export const DeleteResponseCodes = (deleteOp: any, _opts: any, ctx: any) => {
   if (deleteOp === null || typeof deleteOp !== "object") {
--- packages/rulesets/src/spectral/test/delete-response-codes.test.ts
+++ packages/rulesets/src/spectral/test/delete-response-codes.test.ts
@@ -147,6 +147,80 @@ test("DeleteResponseCodes should find errors for sync delete with only 204", () 
   })
 })
 
+test("DeleteResponseCodes should find errors for sync delete without default response", () => {
+  const myOpenApiDocument = {
+    swagger: "2.0",
+    paths: {
+      "/foo": {
+        delete: {
+          tags: ["SampleTag"],
+          operationId: "Foo_Update",
+          description: "Test Description",
+          parameters: [
+            {
+              name: "foo_delete",
+              in: "body",
+              schema: {
+                $ref: "#/definitions/FooRequestParams",
+              },
+            },
+          ],
+          responses: {
+            "204": {
+              description: "No content",
+              schema: {
+                $ref: "#/definitions/FooResource",
+              },
+            },
+            "200": {
+              description: "success",
+              schema: {
+                $ref: "#/definitions/FooResource",
+              },
+            },
+          },
+        },
+      },
+    },
+    definitions: {
+      FooRequestParams: {
+        allOf: [
+          {
+            $ref: "#/definitions/FooProps",
+          },
+        ],
+      },
+      FooResource: {
+        allOf: [
+          {
+            $ref: "#/definitions/FooProps",
+          },
+        ],
+      },
+      FooResourceUpdate: {
+        allOf: [
+          {
+            $ref: "#/definitions/FooProps",
+          },
+        ],
+      },
+      FooProps: {
+        properties: {
+          prop0: {
+            type: "string",
+            default: "my def val",
+          },
+        },
+      },
+    },
+  }
+  return linter.run(myOpenApiDocument).then((results) => {
+    expect(results.length).toBe(1)
+    expect(results[0].path.join(".")).toBe("paths./foo.delete")
+    expect(results[0].message).toContain(SYNC_ERROR)
+  })
+})
+
 test("DeleteResponseCodes should find no errors for sync delete", () => {
   const myOpenApiDocument = {
     swagger: "2.0",
@@ -177,6 +251,10 @@ test("DeleteResponseCodes should find no errors for sync delete", () => {
               schema: {
                 $ref: "#/definitions/FooResource",
               },
+            },
+            default: {
+              description: "Error",
+              schema: {},
             },
           },
         },
@@ -357,6 +435,81 @@ test("DeleteResponseCodes should find errors for lro delete with only 204", () =
   })
 })
 
+test("DeleteResponseCodes should find errors for lro delete without default response", () => {
+  const myOpenApiDocument = {
+    swagger: "2.0",
+    paths: {
+      "/foo": {
+        delete: {
+          tags: ["SampleTag"],
+          operationId: "Foo_Update",
+          description: "Test Description",
+          parameters: [
+            {
+              name: "foo_delete",
+              in: "body",
+              schema: {
+                $ref: "#/definitions/FooRequestParams",
+              },
+            },
+          ],
+          responses: {
+            "202": {
+              description: "accepted",
+              schema: {
+                $ref: "#/definitions/FooResource",
+              },
+            },
+            "204": {
+              description: "No content",
+              schema: {
+                $ref: "#/definitions/FooResource",
+              },
+            },
+          },
+          "x-ms-long-running-operation": true,
+        },
+      },
+    },
+    definitions: {
+      FooRequestParams: {
+        allOf: [
+          {
+            $ref: "#/definitions/FooProps",
+          },
+        ],
+      },
+      FooResource: {
+        allOf: [
+          {
+            $ref: "#/definitions/FooProps",
+          },
+        ],
+      },
+      FooResourceUpdate: {
+        allOf: [
+          {
+            $ref: "#/definitions/FooProps",
+          },
+        ],
+      },
+      FooProps: {
+        properties: {
+          prop0: {
+            type: "string",
+            default: "my def val",
+          },
+        },
+      },
+    },
+  }
+  return linter.run(myOpenApiDocument).then((results) => {
+    expect(results.length).toBe(1)
+    expect(results[0].path.join(".")).toBe("paths./foo.delete")
+    expect(results[0].message).toContain(LR_ERROR)
+  })
+})
+
 test("DeleteResponseCodes should find no errors for lro delete", () => {
   const myOpenApiDocument = {
     swagger: "2.0",
@@ -387,6 +540,10 @@ test("DeleteResponseCodes should find no errors for lro delete", () => {
               schema: {
                 $ref: "#/definitions/FooResource",
               },
+            },
+            default: {
+              description: "Error",
+              schema: {},
             },
           },
           "x-ms-long-running-operation": true,
