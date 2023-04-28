d750daf6ed725ff7b8cb260f40825b84a3f03886
Brett DeFoy <brettdefoy@microsoft.com>
2023-04-27 17:00:15 -0700

    Add check for no patch body parameters

--- packages/rulesets/src/spectral/functions/patch-properties-correspond-to-put-properties.ts
+++ packages/rulesets/src/spectral/functions/patch-properties-correspond-to-put-properties.ts
@@ -14,20 +14,24 @@ export const patchPropertiesCorrespondToPutProperties = (pathItem: any, _opts: a
       path: ctx.path,
     },
   ]
+  // array of all the patch body parameters
   const patchBodyParameters: any[] = pathItem["patch"]?.parameters?.filter(PARAM_IN_BODY)
+  // array of all the put body parameters
   const putBodyParameters: any[] = pathItem["put"]?.parameters?.filter(PARAM_IN_BODY)
 
   // neither patch nor put present => ignore
   if (!patchBodyParameters && !putBodyParameters) {
     return []
   }
-  // patch present but put not present => error
-  if (patchBodyParameters && !putBodyParameters) {
+  // patch present but put not present or there are no patch body parameters => error
+  if ((patchBodyParameters && !putBodyParameters) || patchBodyParameters?.length < 1) {
     return error
   }
 
+  // array of all the patch body parameters that are not present in the put body (if any)
   const patchBodyParametersNotInPutBody = _.differenceWith(patchBodyParameters, putBodyParameters, _.isEqual)
 
+  // there is at least one parameter present in the patch body that is not present in the the put body
   if (patchBodyParametersNotInPutBody.length > 0) {
     return error
   }
