23ed1e66f2789b41bb0ef02682ef83f5efbfb1fc
Brett DeFoy <brettdefoy@microsoft.com>
2023-05-15 16:49:11 -0700

    Move strings to constants

--- packages/rulesets/src/spectral/functions/delete-response-codes.ts
+++ packages/rulesets/src/spectral/functions/delete-response-codes.ts
@@ -3,6 +3,10 @@
 
 const SYNC_DELETE_RESPONSES = ["200", "204", "default"]
 const LR_DELETE_RESPONSES = ["202", "204", "default"]
+const SYNC_ERROR =
+  "Synchronous delete operations must have responses with 200, 204, and default return codes. They also must have no other response codes."
+const LR_ERROR =
+  "Long-running (LRO) delete operations must have responses with 202, 204, and default return codes. They also must have no other response codes."
 
 export const DeleteResponseCodes = (deleteOp: any, _opts: any, ctx: any) => {
   if (deleteOp === null || typeof deleteOp !== "object") {
@@ -20,16 +24,14 @@ export const DeleteResponseCodes = (deleteOp: any, _opts: any, ctx: any) => {
   if (deleteOp["x-ms-long-running-operation"] === true) {
     if (responses.length !== LR_DELETE_RESPONSES.length || !LR_DELETE_RESPONSES.every((value) => responses.includes(value))) {
       errors.push({
-        message:
-          "Long-running (LRO) delete operations must have responses with 202, 204, and default return codes. They also must have no other response codes.",
+        message: LR_ERROR,
         path: path,
       })
     }
   } else {
     if (responses.length !== SYNC_DELETE_RESPONSES.length || !SYNC_DELETE_RESPONSES.every((value) => responses.includes(value))) {
       errors.push({
-        message:
-          "Synchronous delete operations must have responses with 200, 204, and default return codes. They also must have no other response codes.",
+        message: SYNC_ERROR,
         path: path,
       })
     }
