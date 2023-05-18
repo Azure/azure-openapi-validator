228a1557eb8aafa32017cde0deaf3399d7f3d6f5
rkmanda <rmanda@microsoft.com>
2023-05-17 18:04:07 -0700

    fixing rush build inconsistency on main (#521)
    * fixing rush build inconsistency on main

--- packages/rulesets/generated/spectral/az-arm.js
+++ packages/rulesets/generated/spectral/az-arm.js
@@ -1447,6 +1447,8 @@ const consistentPatchProperties = (patchOp, _opts, ctx) => {
 
 const SYNC_DELETE_RESPONSES = ["200", "204", "default"];
 const LR_DELETE_RESPONSES = ["202", "204", "default"];
+const SYNC_ERROR = "Synchronous delete operations must have responses with 200, 204, and default return codes. They also must have no other response codes.";
+const LR_ERROR = "Long-running (LRO) delete operations must have responses with 202, 204, and default return codes. They also must have no other response codes.";
 const DeleteResponseCodes = (deleteOp, _opts, ctx) => {
     var _a;
     if (deleteOp === null || typeof deleteOp !== "object") {
@@ -1461,7 +1463,7 @@ const DeleteResponseCodes = (deleteOp, _opts, ctx) => {
     if (deleteOp["x-ms-long-running-operation"] === true) {
         if (responses.length !== LR_DELETE_RESPONSES.length || !LR_DELETE_RESPONSES.every((value) => responses.includes(value))) {
             errors.push({
-                message: "Long-running (LRO) delete operations must have responses with 202, 204, and default return codes. They also must have no other response codes.",
+                message: LR_ERROR,
                 path: path,
             });
         }
@@ -1469,7 +1471,7 @@ const DeleteResponseCodes = (deleteOp, _opts, ctx) => {
     else {
         if (responses.length !== SYNC_DELETE_RESPONSES.length || !SYNC_DELETE_RESPONSES.every((value) => responses.includes(value))) {
             errors.push({
-                message: "Synchronous delete operations must have responses with 200, 204, and default return codes. They also must have no other response codes.",
+                message: SYNC_ERROR,
                 path: path,
             });
         }
