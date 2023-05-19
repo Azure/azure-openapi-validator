6b95b9752a8ca3f4fe50b90cca1802151162f3bd
tejaswiMinnu <tsalaigari@microsoft.com>
2023-05-19 14:22:03 -0400

    Check if the property of type object has definition or not (#516)
    * files to check if property contains type object definition
    
    * change log generated file
    
    * addressed PR comments
    
    * changed the error message
    
    * Added real swagger snippets
    
    * md file example
    
    * addressed PR comments

---
+++ packages/rulesets/src/spectral/functions/properties-type-object-no-definition.ts
@@ -0,0 +1,22 @@
+// If a property is defined with `'type': 'object'`, it must also define an object body.
+// RPC Code: RPC-Policy-V1-03
+const errorMessageObject =
+  "Properties with type:object that don't reference a model definition are not allowed. ARM doesn't allow generic type definitions as this leads to bad customer experience."
+const errorMessageNull =
+  "Properties with type NULL are not allowed. Either specify the type as object and reference a model or specify a primitive type."
+export const propertiesTypeObjectNoDefinition: any = (definitionObject: any, opts: any, ctx: any) => {
+  const path = ctx.path || []
+  const errors = []
+  if (definitionObject?.type === "") {
+    errors.push({ message: errorMessageNull, path })
+  }
+  const values = Object.values(definitionObject)
+  for (const val of values) {
+    if (typeof val === "object") return []
+    else continue
+  }
+  if (definitionObject?.type === "object") {
+    errors.push({ message: errorMessageObject, path })
+  }
+  return errors
+}
