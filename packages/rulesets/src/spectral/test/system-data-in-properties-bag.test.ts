a427f42f9760d80f7454fabbfa1d619c535e595c
Brett DeFoy <brettdefoy@microsoft.com>
2023-05-24 10:52:29 -0700

    WIP adding test cases

--- packages/rulesets/src/spectral/test/system-data-in-properties-bag.test.ts
+++ packages/rulesets/src/spectral/test/system-data-in-properties-bag.test.ts
@@ -10,7 +10,7 @@ beforeAll(async () => {
   return nonResolvingLinter
 })
 
-test(`${RULE} should find errors`, async () => {
+test(`${RULE} should find errors for system data in properties bag`, async () => {
   const oasDoc = {
     swagger: "2.0",
     paths: {},
@@ -38,6 +38,36 @@ test(`${RULE} should find errors`, async () => {
   })
 })
 
+test(`${RULE} should find errors for system data in nested properties`, async () => {
+  const oasDoc = {
+    swagger: "2.0",
+    paths: {},
+    definitions: {
+      Resource: {
+        properties: {
+          properties: {
+            properties: {
+              systemData: {
+                $ref: "../../../../../common-types/resource-management/v2/types.json#/definitions/systemData",
+              },
+              SystemData: {
+                $ref: "../../../../../common-types/resource-management/v2/types.json#/definitions/systemData",
+              },
+            },
+          },
+        },
+      },
+    },
+  }
+  return nonResolvingLinter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(2)
+    expect(results[0].path.join(".")).toBe("definitions.Resource.properties.properties.properties.systemData")
+    expect(results[0].message).toBe(ERROR_MESSAGE)
+    expect(results[1].path.join(".")).toBe("definitions.Resource.properties.properties.properties.SystemData")
+    expect(results[1].message).toBe(ERROR_MESSAGE)
+  })
+})
+
 test(`${RULE} should find no errors`, async () => {
   const oasDoc = {
     swagger: "2.0",
@@ -60,3 +90,5 @@ test(`${RULE} should find no errors`, async () => {
     expect(results.length).toBe(0)
   })
 })
+
+// add test cases for nested
