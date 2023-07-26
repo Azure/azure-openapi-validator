1043105b10f9690d01488f57106ad803a35c8347
Tejaswi Salaigari <tsalaigari@microsoft.com>
2023-07-26 11:25:54 -0400

    removed extra line

--- packages/rulesets/src/spectral/functions/properties-type-object-no-definition.ts
+++ packages/rulesets/src/spectral/functions/properties-type-object-no-definition.ts
@@ -1,11 +1,10 @@
 // If a property is defined with `'type': 'object'`, it must also define an object body.
-
 // RPC Code: RPC-Policy-V1-03
 const errorMessageObject =
   "Properties with type:object that don't reference a model definition are not allowed. ARM doesn't allow generic type definitions as this leads to bad customer experience."
 const errorMessageNull =
   "Properties with type NULL are not allowed. Either specify the type as object and reference a model or specify a primitive type."
-export const propertiesTypeObjectNoDefinition: any = (definitionObject: any, _opts: any, ctx: any) => {
+export const propertiesTypeObjectNoDefinition: any = (definitionObject: any, opts: any, ctx: any) => {
   const path = ctx.path || []
 
   if (definitionObject?.properties) {
