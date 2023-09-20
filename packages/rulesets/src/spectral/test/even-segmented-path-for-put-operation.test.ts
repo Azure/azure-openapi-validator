b6571f7877444a0cfec85d37dbda64de0e076a00
akhilailla <akhilailla@microsoft.com>
2023-09-20 11:54:28 -0500

    split tests based on the cases

--- packages/rulesets/src/spectral/test/even-segmented-path-for-put-operation.test.ts
+++ packages/rulesets/src/spectral/test/even-segmented-path-for-put-operation.test.ts
@@ -8,29 +8,10 @@ beforeAll(async () => {
   return linter
 })
 
-test("PathForResourceAction should find errors for invalid path and no errors for valid path", () => {
+test("PathForResourceAction should find errors for invalid paths", () => {
   const oasDoc = {
     swagger: "2.0",
     paths: {
-      //valid uri's
-      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}": {
-        put: {
-          tags: ["SampleTag"],
-          operationId: "Foo_CreateOrUpdate",
-          description: "Test Description",
-          parameters: [],
-          responses: {},
-        },
-      },
-      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}": {
-        put: {
-          tags: ["SampleTag"],
-          operationId: "Foo_CreateOrUpdate",
-          description: "Test Description",
-          parameters: [],
-          responses: {},
-        },
-      },
       //invalid uri's
       "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music": {
         put: {
@@ -98,5 +79,35 @@ test("PathForResourceAction should find errors for invalid path and no errors fo
     expect(results[4].path.join(".")).toBe(
       "paths./subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}/addSong"
     )
+  })
+})
+
+test("PathForResourceAction should not find errors for valid path", () => {
+  const oasDoc = {
+    swagger: "2.0",
+    paths: {
+      //valid uri's
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}": {
+        put: {
+          tags: ["SampleTag"],
+          operationId: "Foo_CreateOrUpdate",
+          description: "Test Description",
+          parameters: [],
+          responses: {},
+        },
+      },
+      "/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Music/Songs/{songName}/Artist/{artistName}": {
+        put: {
+          tags: ["SampleTag"],
+          operationId: "Foo_CreateOrUpdate",
+          description: "Test Description",
+          parameters: [],
+          responses: {},
+        },
+      },
+    },
+  }
+  return linter.run(oasDoc).then((results) => {
+    expect(results.length).toBe(0)
   })
 })
