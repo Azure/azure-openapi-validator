1281d6649d333592822c1a7a571e7a6e8bc9c785
Roopesh Manda <rmanda@microsoft.com>
2023-12-08 18:46:58 -0800

    Fix dcumentation and add tests for the AvoidNestedProperties rule

--- docs/avoid-nested-properties.md
+++ docs/avoid-nested-properties.md
@@ -20,6 +20,101 @@ Nested properties can result into bad user experience especially when creating r
 
 Overly nested properties (especially required ones) can result into a non optimal user experience.
 
-## How to fix the violation
+## How to fix
 
 Either eliminate nesting or use the `x-ms-client-flatten` property for a better user experience. More details about the extension can be found [here](https://github.com/Azure/autorest/blob/main/docs/extensions/readme.md#x-ms-client-flatten).
+
+## Bad examples
+
+## Bad example 1
+Deeply nested properties exist and the x-ms-client-flatten extension is not specified
+
+```json
+    definitions: {
+      externalDocs: {
+        type: "object",
+        properties: {
+          tags: {
+            type: "object",
+            additionalProperties: {
+              type: "string",
+            },
+            description: "Resource tags.",
+          },
+          properties: {
+            type: "object",
+            properties: {
+              timeOfDayUTC: {
+                type: "string",
+                pattern: "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+                description: "The time of day (in UTC) to start the maintenance window.",
+              },
+            },
+          },
+        },
+      },
+    },
+```
+
+## Bad example 2
+Deeply nested properties exist and the x-ms-client-flatten extension is specified in the schema object and not against the complex type
+
+```json
+    definitions: {
+      externalDocs: {
+        type: "object",
+        properties: {
+          tags: {
+            type: "object",
+            additionalProperties: {
+              type: "string",
+            },
+            description: "Resource tags.",
+          },
+          properties: {
+            type: "object",
+            // x-ms-client-flatten should have been specified here
+            properties: {
+              "x-ms-client-flatten": true,
+              timeOfDayUTC: {
+                type: "string",
+                pattern: "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+                description: "The time of day (in UTC) to start the maintenance window.",
+              },
+            },
+          },
+        },
+      },
+    },
+```
+## Good examples
+
+Deeply nested properties exist and the x-ms-client-flatten extension is specified against the complex type
+
+```json
+    definitions: {
+      externalDocs: {
+        type: "object",
+        properties: {
+          tags: {
+            type: "object",
+            additionalProperties: {
+              type: "string",
+            },
+            description: "Resource tags.",
+          },
+          properties: {
+            type: "object",
+            "x-ms-client-flatten": true,
+            properties: {
+              timeOfDayUTC: {
+                type: "string",
+                pattern: "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+                description: "The time of day (in UTC) to start the maintenance window.",
+              },
+            },
+          },
+        },
+      },
+    },
+```
--- packages/rulesets/src/spectral/test/avoid-nested-properties.test.ts
+++ packages/rulesets/src/spectral/test/avoid-nested-properties.test.ts
@@ -17,17 +17,25 @@ test("AvoidNestedProperties should find errors", () => {
     },
     definitions: {
       externalDocs: {
+        type: "object",
         properties: {
+          tags: {
+            type: "object",
+            additionalProperties: {
+              type: "string",
+            },
+            description: "Resource tags.",
+          },
           properties: {
+            type: "object",
             properties: {
-              $ref: "#/definitions/BackendProperties",
+              timeOfDayUTC: {
+                type: "string",
+                pattern: "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+                description: "The time of day (in UTC) to start the maintenance window.",
+              },
             },
           },
-        },
-      },
-      BackendProperties: {
-        properties: {
-          $ref: "#/definitions/externalDocs",
         },
       },
     },
@@ -48,6 +56,7 @@ test("AvoidNestedProperties should find no errors", () => {
       externalDocs: {
         properties: {
           properties: {
+            type: "object",
             "x-ms-client-flatten": true,
             $ref: "#/definitions/BackendProperties",
           },
@@ -56,6 +65,82 @@ test("AvoidNestedProperties should find no errors", () => {
       BackendProperties: {
         properties: {
           $ref: "#/definitions/externalDocs",
+        },
+      },
+    },
+  }
+  return linter.run(myOpenApiDocument).then((results) => {
+    expect(results.length).toBe(0)
+  })
+})
+
+test("AvoidNestedProperties should find errors when the extension is specified outside the complex type definition", () => {
+  const myOpenApiDocument = {
+    swagger: "2.0",
+    info: {
+      description:
+        "Use these APIs to manage Azure Websites resources through the Azure Resource Manager. All task operations conform to the HTTP/1.1 protocol specification and each operation returns an x-ms-request-id header that can be used to obtain information about the request. You must make sure that requests made to these resources are secure. For more information, see https://msdn.microsoft.com/en-us/library/azure/dn790557.aspx.",
+    },
+    definitions: {
+      externalDocs: {
+        type: "object",
+        properties: {
+          tags: {
+            type: "object",
+            additionalProperties: {
+              type: "string",
+            },
+            description: "Resource tags.",
+          },
+          properties: {
+            type: "object",
+            properties: {
+              "x-ms-client-flatten": true,
+              timeOfDayUTC: {
+                type: "string",
+                pattern: "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+                description: "The time of day (in UTC) to start the maintenance window.",
+              },
+            },
+          },
+        },
+      },
+    },
+  }
+  return linter.run(myOpenApiDocument).then((results) => {
+    expect(results.length).toBe(1)
+  })
+})
+
+test("AvoidNestedProperties should not find errors when the extension is specified for a complex type definition", () => {
+  const myOpenApiDocument = {
+    swagger: "2.0",
+    info: {
+      description:
+        "Use these APIs to manage Azure Websites resources through the Azure Resource Manager. All task operations conform to the HTTP/1.1 protocol specification and each operation returns an x-ms-request-id header that can be used to obtain information about the request. You must make sure that requests made to these resources are secure. For more information, see https://msdn.microsoft.com/en-us/library/azure/dn790557.aspx.",
+    },
+    definitions: {
+      externalDocs: {
+        type: "object",
+        properties: {
+          tags: {
+            type: "object",
+            additionalProperties: {
+              type: "string",
+            },
+            description: "Resource tags.",
+          },
+          properties: {
+            type: "object",
+            "x-ms-client-flatten": true,
+            properties: {
+              timeOfDayUTC: {
+                type: "string",
+                pattern: "^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$",
+                description: "The time of day (in UTC) to start the maintenance window.",
+              },
+            },
+          },
         },
       },
     },
