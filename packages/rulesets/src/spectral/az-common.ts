d277c5ba45dfbab7e63aed999d9432813811d639
Travis Prescott <tjprescott@users.noreply.github.com>
2024-04-03 12:30:16 -0700

    Disable LintDiff rules for TypeSpec 0.55 (#669)
    * Disable ResourceNameRestriction LintDiff rule.
    
    * Disable rules for TypeSpec.
    
    * Bump ruleset version.

--- packages/rulesets/src/spectral/az-common.ts
+++ packages/rulesets/src/spectral/az-common.ts
@@ -412,6 +412,8 @@ const ruleset: any = {
       message:
         "'{{property}}' parameter lacks 'description' property. Consider adding a 'description' element. Accurate description is essential for maintaining reference documentation.",
       severity: "error",
+      disableForTypeSpec: true,
+      disableForTypeSpecReason: "Covered by TSP's '@azure-tools/typespec-azure-core/documentation-required' rule.",
       resolved: false,
       formats: [oas2],
       given: ["$.parameters.*"],
