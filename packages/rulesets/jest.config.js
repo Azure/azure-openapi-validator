5586fe14324f6c0003c86274ca1a29aa6b38da51
Mike Harder <mharder@microsoft.com>
2024-04-30 22:27:33 +0000

    Remove old nimma config

--- packages/rulesets/jest.config.js
+++ packages/rulesets/jest.config.js
@@ -42,8 +42,4 @@ module.exports = {
       "statements": 80
     }
   },
-  moduleNameMapper: {
-    "^nimma/legacy$": "<rootDir>/node_modules/nimma/dist/legacy/cjs/index.js",
-    "^nimma/(.*)": "<rootDir>/node_modules/nimma/dist/cjs/$1"
-  },
 }
