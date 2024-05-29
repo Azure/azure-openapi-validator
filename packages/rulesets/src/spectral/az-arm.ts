0cf84754357ae7b8c8d66818bdb74331ef02dbe9
akhilailla <akhilailla@microsoft.com>
2024-05-29 08:26:24 -0500

    remove commented line

--- packages/rulesets/src/spectral/az-arm.ts
+++ packages/rulesets/src/spectral/az-arm.ts
@@ -418,8 +418,7 @@ const ruleset: any = {
       message: "{{error}}",
       resolved: true,
       formats: [oas2],
-      //[?(@property === '200' || @property === '201')]
-      given: "$[paths,'x-ms-paths'][?(!@property.endsWith('}') && !@property.endsWith('default'))].get",
+      given: "$[paths,'x-ms-paths'][?(!@property.endsWith('}') && !@property.endsWith('/default'))].get",
       then: {
         function: xmsPageableForListCalls,
       },
