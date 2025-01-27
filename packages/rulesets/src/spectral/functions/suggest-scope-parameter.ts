9a7c8bca90207e1467774172213d74b7c1f061b9
Brett DeFoy <brettdefoy@microsoft.com>
2025-01-27 14:58:23 -0700

    broken

--- packages/rulesets/src/spectral/functions/suggest-scope-parameter.ts
+++ packages/rulesets/src/spectral/functions/suggest-scope-parameter.ts
@@ -4,23 +4,52 @@
  * This function checks if the given path can be combined with other paths in the Swagger document
  * by introducing a scope parameter. It returns suggestions for paths that differ only in scope.
  */
-const suggestScopeParameter = (path: any, _opts: any, ctx: any) => {
-  const swagger = ctx?.documentInventory?.resolved
+import _ from "lodash"
 
-  if (path === null || typeof path !== "string" || path.length === 0 || swagger === null || path.startsWith("{scope}")) {
+const suggestScopeParameter = (paths: any, _opts: any, ctx: any) => {
+  if (paths === null || typeof paths !== "object") {
     return []
   }
 
-  const resourceTypeName = path.substring(path.lastIndexOf("/providers"))
+  const entries = Object.entries(paths)
+  if (
+    !Array.isArray(entries) ||
+    entries.length === 0 ||
+    !entries.every((p) => Array.isArray(p)) ||
+    !entries.every((p) => p.length === 2) ||
+    !entries.every((p) => typeof p[0] === "string") ||
+    !entries.every((p) => typeof p[1] === "object")
+  ) {
+    return []
+  }
 
-  const otherPaths = Object.keys(swagger.paths).filter((p: string) => p !== path && p.endsWith(resourceTypeName))
+  // Find paths that do not start with the scope parameter
+  const scopedPaths = entries.filter((p: Array<any>) => !p[0].startsWith("{scope}"))
 
-  return otherPaths.map((p) => {
-    return {
-      message: `Path "${p}" differs from path "${path}" only in scope. These paths can be combined by defining a scope parameter.`,
-      path: ctx.path,
+  const errors = []
+
+  for (const scopedPath of scopedPaths) {
+    // Find other paths that differ only in scope
+    const otherPaths = scopedPaths.filter(
+      (p: Array<any>) =>
+        p[0] !== scopedPath[0] &&
+        // Paths have the same "/providers/..." suffix
+        p[0].endsWith(scopedPath[0].substring(scopedPath[0].indexOf("/providers"))),
+    )
+
+    if (otherPaths.length > 0) {
+      errors.push(
+        otherPaths.map((p) => {
+          return {
+            message: `Path "${p}" differs from path "${scopedPath}" only in scope. These paths can be combined by defining a scope parameter.`,
+            path: ctx.path,
+          }
+        }),
+      )
     }
-  })
+  }
+
+  return errors
 }
 
 export default suggestScopeParameter
