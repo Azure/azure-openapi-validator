eb7cf8b499ca0727d4dd45b58f208a7b68a65b04
Mike Harder <mharder@microsoft.com>
2025-10-30 20:39:18 +0000

    Revert "remove unnecessary expect-error"
    This reverts commit dfb58f8f9721d369562a83a910eeffa5401d77ad.

--- .github/vitest.config.js
+++ .github/vitest.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   esbuild: {
     // Ignore tsconfig.json, since it's only used for type checking, and causes
     // a warning if vitest tries to load it
+
+    // @ts-expect-error: 'tsConfig' does not exist in type 'ESBuildOptions'
     tsConfig: false,
   },
 
