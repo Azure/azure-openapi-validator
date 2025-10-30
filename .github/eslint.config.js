21659f5f808de4a5785bcd99316a851335e1fb54
Mike Harder <mharder@microsoft.com>
2025-10-30 20:37:16 +0000

    Revert "enable type checked ruleset by default, disable for tests"
    This reverts commit 6866119f40797e3e4f89fca79da7a004f38f3932.

--- .github/eslint.config.js
+++ .github/eslint.config.js
@@ -6,20 +6,6 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
-export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      globals: globals.node,
-      parserOptions: {
-        projectService: true,
-      },
-    },
-  },
-  {
-    // must enable TSC checking of tests, before we can enable eslint type-checked
-    files: ["**/test/**/*.js"],
-    extends: [tseslint.configs.disableTypeChecked],
-  },
-);
+export default defineConfig(eslint.configs.recommended, tseslint.configs.recommended, {
+  languageOptions: { globals: globals.node },
+});
